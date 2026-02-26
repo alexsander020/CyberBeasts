24d2a336431c8ad07d49d930d9e8f0705bfd1f59
Alexsander Sudario <92872511+alexsander020@users.noreply.github.com>
2026-02-25 21:03:08 -0300

    update

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/CyberBeasts/', // Substitua pelo nome do seu repositório no GitHub
+    build: {
+        outDir: 'dist',
+    }
+})
